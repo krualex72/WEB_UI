025d53832e39b59625f15437cc08a7a355e9cd56
krutikov-alex <krutikov-alex@ya.ru>
2022-03-03 17:51:38 +0300

    Work is in progress...

---
+++ MainApp/src/HomeWork7/MainApp.java
@@ -0,0 +1,9 @@
+package HomeWork7;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
